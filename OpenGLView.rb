38f667b575a0ab560833ae344a9831b00776ef71
Brrrutal <slikhan@gmail.com>
2013-01-10 00:42:27 +0200

    Added basic OpenGL view.

---
+++ OpenGLView.rb
@@ -0,0 +1,37 @@
+#
+#  OpenGLView.rb
+#  OpenGL
+#
+#  Created by Serhiy Likhanov on 12/16/12.
+#  Copyright 2012 Serhiy Likhanov. All rights reserved.
+#
+
+class OpenGLView < NSOpenGLView
+
+    @@color = 0.001
+    
+    def reshape
+        rect = bounds
+        glViewport(0, 0, NSWidth(rect), NSHeight(rect))
+    end
+    
+    def drawRect(rect)
+        @@color += 0.001
+        glClearColor(0, @@color, 0, 1)
+        glClear(GL_COLOR_BUFFER_BIT)
+        
+        drawAnObject
+        
+        openGLContext.flushBuffer
+    end
+    
+    def drawAnObject
+        glColor3f(1.0, 0.85, 0.35)
+        glBegin(GL_TRIANGLES)
+
+        glVertex3f(  0.0,  0.6, 0.0)
+        glVertex3f( -0.2, -0.3, 0.0)
+        glVertex3f(  0.2, -0.3 ,0.0)
+        glEnd()
+    end
+end
--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@
 
 framework 'Cocoa'
 
+require 'OpenGLView'
+
 app = NSApplication.sharedApplication
 
 window_frame = [0.0, 0.0, 800.0, 600.0]
@@ -12,9 +14,14 @@ main_window = NSWindow.alloc.initWithContentRect window_frame,
         backing: NSBackingStoreBuffered,
         defer: false
 
+content_view = NSView.alloc.initWithFrame window_frame
+main_window.contentView = content_view
+
+gl_view = OpenGLView.alloc.initWithFrame window_frame
+content_view.addSubview gl_view
+
 main_window.display
 main_window.makeKeyAndOrderFront nil
 main_window.orderFrontRegardless
-puts "Before run"
 app.run
 
