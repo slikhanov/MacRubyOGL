d6532ecd07ba1f58a0e6f2739c4842f947651993
Brrrutal <slikhan@gmail.com>
2013-01-11 00:39:29 +0200

    Fixed runtime NSOpenGLView initialization.

--- main.rb
+++ main.rb
@@ -17,7 +17,23 @@ main_window = NSWindow.alloc.initWithContentRect window_frame,
 content_view = NSView.alloc.initWithFrame window_frame
 main_window.contentView = content_view
 
-gl_view = OpenGLView.alloc.initWithFrame window_frame
+# Identifying pixel format
+attributes_source = [
+    #NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core, 
+    NSOpenGLPFAColorSize, 24,
+    NSOpenGLPFAAlphaSize, 8,
+    NSOpenGLPFAAccelerated,
+    NSOpenGLPFADoubleBuffer,
+    0 ]
+
+attributes = Pointer.new_with_type(:uint, attributes_source.size)
+(0..attributes_source.size - 1).each do |i|
+    attributes[i] = attributes_source[i]
+end
+
+pixel_format = NSOpenGLPixelFormat.alloc.initWithAttributes attributes
+
+gl_view = OpenGLView.alloc.initWithFrame window_frame, pixelFormat: pixel_format
 content_view.addSubview gl_view
 
 main_window.display
