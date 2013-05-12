10f859a68f14ea8e32d1530c86c15492917862f2
Brrrutal <slikhan@gmail.com>
2013-05-13 00:06:15 +0300

    VertexBufferObject class
    1. Introduced VertexBufferObject class.
    2. Cleaned up Mesh class, probably it will be removed in the future as
    it doesn't carry enough information/functionality.

--- Mesh.rb
+++ Mesh.rb
@@ -20,20 +20,37 @@ end
 # Class for preparing buffer data
 class BufferData
   attr_reader :data
-  attr_reader :size
+  attr_reader :count
+  attr_reader :components
 
-  def initialize(buffer_data)
-    @size = buffer_data.count
+  def initialize(buffer_data, components)
+    @components = components
+    @count = buffer_data.count
     @data = Pointer.new(:float, buffer_data.count)
     (0..buffer_data.count - 1).each do |idx|
       @data[idx] = buffer_data[idx]
     end
   end
+
+  def size
+    sizeof(:float) * count
+  end
 end
 
 # Class that manages Vertex Buffer Objects
 class VertexBufferObject
-  def initialize(buffer_data)
+  def initialize(buffer_data, attribute_index)
+    vbo_objects_array = Pointer.new_with_type("I", 1)
+    glGenBuffers(1, vbo_objects_array)
+    @vbo = vbo_objects_array.value
+    update(buffer_data, attribute_index)
+  end
+
+  def update(buffer_data, attribute_index)
+    glBindBuffer(GL_ARRAY_BUFFER, @vbo)
+    glBufferData(GL_ARRAY_BUFFER, buffer_data.size, buffer_data.data, GL_STATIC_DRAW)
+    glEnableVertexAttribArray(attribute_index)
+    glVertexAttribPointer(attribute_index, buffer_data.components, GL_FLOAT, GL_FALSE, 0, Pointer.magic_cookie(0))
   end
 end
 
@@ -43,25 +60,14 @@ class Mesh
       @vao = VertexArrayObject.new
       @vao.enable
 
-      vbo_objects_array = Pointer.new_with_type("I", 1)
-      glGenBuffers(1, vbo_objects_array)
-      puts glGetError
-      @vbo = vbo_objects_array.value
-      glBindBuffer(GL_ARRAY_BUFFER, @vbo)
       @data = BufferData.new([
         -0.3, -0.2,
         0.0, 0.0,
         0.1, 0.1,
         0.2, 0.4,
         -0.3, 0.4
-      ])
-      glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(:float), @data.data, GL_STATIC_DRAW)
-
-      glEnableVertexAttribArray(program.position)
-      puts glGetError
-      glVertexAttribPointer(program.position, 2, GL_FLOAT, GL_FALSE, 0, Pointer.magic_cookie(0))
-      puts glGetError
-
+      ], 2)
+      vbo = VertexBufferObject.new(@data, 0)
       @vao.disable
     end
 
